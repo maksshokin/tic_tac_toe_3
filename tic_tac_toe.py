4b1a9598ba647c6974fe335476b114afa49bd217
Maksim Shokin <maksshokin90@gmail.com>
2024-03-01 19:48:39 +0300

    may be just end this soon

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,5 +1,4 @@
 import pygame
-import random
 
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
@@ -7,15 +6,33 @@ pygame.display.set_caption("TICTACTOE")
 icon =pygame.image.load('Images/icon.png')
 pygame.display.set_icon(icon)
 
+clock = pygame.time.Clock()
+
+def filling_black(object):
+    object.fill((0, 0, 0))
+
 line_gorizontal = pygame.Surface((600, 1))
-line_gorizontal.fill((0, 0, 0))
 line_vertical = pygame.Surface((1, 600))
-line_vertical.fill((0, 0, 0))
+filling_black(line_gorizontal)
+filling_black(line_vertical)
+
+coordinate = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+#bg_sound = pygame.mixer.Sound('sounds/bg.mp3')
+#bg_sound.play()
+
+def Tic(coordinates):
+    pygame.draw.circle(surface=screen, color=(0, 0, 0), width=1, radius=100, center=(coordinates))
+
+def Tac(coordinates):
+    pass
+
+Tic((150, 150))
 
 running = True
 while running:
     screen.fill((210,180,230))
-
+    clock.tick(60)
     screen.blit(line_gorizontal, (0, 200))
     screen.blit(line_gorizontal, (0, 400))
     screen.blit(line_vertical, (200, 0))
@@ -25,5 +42,12 @@ while running:
         if event.type == pygame.QUIT:
             pygame.QUIT
             running = False
+        elif event.type == pygame.MOUSEMOTION:
+            cursor_pos = event.pos
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                print(cursor_pos)
+                Tic((100, 100))
+                
     pygame.display.update()
 
