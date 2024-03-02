ed31c465cb9919c46735d7360ba0883b42874d82
Maksim Shokin <maksshokin90@gmail.com>
2024-03-02 08:10:41 +0300

    soon...

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -16,18 +16,32 @@ line_vertical = pygame.Surface((1, 600))
 filling_black(line_gorizontal)
 filling_black(line_vertical)
 
-coordinate = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
 
 #bg_sound = pygame.mixer.Sound('sounds/bg.mp3')
 #bg_sound.play()
 
 def Tic(coordinates):
-    pygame.draw.circle(surface=screen, color=(0, 0, 0), width=1, radius=100, center=(coordinates))
+    """Отрисовка круга с центром на заданных координатах."""
+    pygame.draw.circle(surface=screen, color=(0, 0, 0), width=1, radius=75, center=(coordinates))
 
 def Tac(coordinates):
-    pass
+    """Отрисовка крестика с центром на заданных координатах."""
+    st_coord_1 = coordinates[0] - 50
+    st_coord_2 = coordinates[1] - 50
+    ed_coord_1 = coordinates[0] + 50
+    ed_coord_2 = coordinates[1] + 50
+    start_pose_1 = (st_coord_1, st_coord_2)
+    end_pose_1   = (ed_coord_1, ed_coord_2)
+    start_pose_2 = (st_coord_1, ed_coord_2)
+    end_pose_2   = (ed_coord_1, st_coord_2)
 
-Tic((150, 150))
+    pygame.draw.line(surface=screen,color=(0, 0, 0), start_pos=start_pose_1, end_pos=end_pose_1)
+    pygame.draw.line(surface=screen,color=(0, 0, 0), start_pos=start_pose_2, end_pos=end_pose_2)
+
+
+
+coordinate = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 running = True
 while running:
@@ -38,6 +52,8 @@ while running:
     screen.blit(line_vertical, (200, 0))
     screen.blit(line_vertical, (400, 0))
     
+    count = 1
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.QUIT
@@ -47,7 +63,29 @@ while running:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 print(cursor_pos)
-                Tic((100, 100))
+                if cursor_pos[0] < 200:
+                    if cursor_pos[1] < 200:
+                        Tac((100, 100))
+                    elif cursor_pos[1] < 400:
+                        Tac((100,300))
+                    else:
+                        Tac((100, 500))
+                elif cursor_pos[0] < 400:
+                    if cursor_pos[1] < 200:
+                       Tac((300, 100))
+                    elif cursor_pos[1] < 400:
+                       Tac((300,300))
+                    else:
+                       Tac((300, 500))
+                else:
+                    if cursor_pos[1] < 200:
+                        Tac((500, 100))
+                    elif cursor_pos[1] < 400:
+                        Tac((500,300))
+                    else:
+                        Tac((500, 500))
                 
+                
+
     pygame.display.update()
 
